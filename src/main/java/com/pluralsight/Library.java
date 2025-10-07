185d6a3c80ff07fb25d9ce8f3e6a97617687b03b
Romell Walker <walkerromell@gmail.com>
2025-10-07 09:46:59 -0400

    set up checked out variables

--- src/main/java/com/pluralsight/Library.java
+++ src/main/java/com/pluralsight/Library.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Library {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
 }
